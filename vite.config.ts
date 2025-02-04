fe57021552ccb2882993de388e392e74124ed32d
Rodolfo Barbeito <rbarbeito@gmail.com>
2025-02-04 08:59:26 +0100

    fix vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -17,14 +17,6 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
+
   }
 })
